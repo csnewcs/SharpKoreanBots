045179a602a745de3cb6378235bca94fff7a8649
csnewcs <cs.newcs0@gmail.com>
2021-10-18 18:07:57 +0900

    유저정보 가져오기

---
+++ SharpKoreanBots/example/3. Get User Info/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SharpKoreanBots.User;
+
+namespace _3._Get_User_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = ulong.Parse(System.IO.File.ReadAllLines("../token.txt")[2]);
+            UserInfo userInfo = UserInfo.Get(id);
+            string bots = "";
+            foreach(var bot in userInfo.Bots)
+            {
+                bots += bot.ToString() + "\n";
+            }
+            Console.WriteLine($"===============유저 정보===============\n이름#태그: {userInfo.Name}#{userInfo.Tag}\n깃헙: {userInfo.GitHub}\n봇들: {bots}");
+        }
+    }
+}
--- SharpKoreanBots/src/User/UserInfo.cs
+++ SharpKoreanBots/src/User/UserInfo.cs
@@ -13,11 +13,31 @@ namespace SharpKoreanBots.User
         const string baseUrl = "https://koreanbots.dev/api/v2/";
 
         ulong _id;
+        public ulong ID
+        {
+            get {return _id;}
+        }
         string _name;
+        public string Name
+        {
+            get => _name;
+        }
         int _tag;
+        public int Tag
+        {
+            get => _tag;
+        }
         string _github;
+        public string GitHub
+        {
+            get => _github;
+        }
         UserFlag _flag;
         BotInfo[] _bots;
+        public BotInfo[] Bots
+        {
+            get => _bots;
+        }
         public UserInfo(ulong id, string name = null, int tag = 0, string github = null, UserFlag flag = UserFlag.Normal, BotInfo[] bots = null)
         {
             _id = id;
@@ -37,7 +57,28 @@ namespace SharpKoreanBots.User
             {
                 throw new Exception($"Server returned code {json["code"]}\nMessage: {json["message"]}");
             }
-            return new UserInfo(UserID);
+            JObject data = (JObject)json["data"];
+
+            List<BotInfo> bots = new List<BotInfo>();
+            foreach(var bot in data["bots"])
+            {
+                List<UserInfo> owners = new List<UserInfo>();
+                foreach(var owner in bot["owners"])
+                {
+                    owners.Add(new UserInfo((ulong)owner));
+                }
+                bots.Add(new BotInfo((ulong)bot["id"], null, bot["name"].ToString(), (int)bot["tag"], owners.ToArray(), bot["prefix"]?.ToString(), (int)bot["votes"], bot["intro"]?.ToString(), bot["desc"]?.ToString(), bot["lib"]?.ToString(), BotInfo.GetBotState(bot["state"]?.ToString()), bot["servers"].Type == JTokenType.Null ? 0 : (int)bot["servers"], bot["shards"].Type == JTokenType.Null ? 0 : (int)bot["shards"], bot["web"]?.ToString(), bot["github"]?.ToString(), bot["discord"]?.ToString(), bot["avatar"]?.ToString())); 
+            }
+
+            return new UserInfo(
+                (ulong)data["id"],
+                (string)data["username"],
+                (int)data["tag"],
+                data["github"]?.ToString(),
+                bots: bots.ToArray()
+                // (UserFlag)data["flag"],
+                // BotInfo.Get((ulong)data["id"])
+            );
         }
         public override string ToString()
         {
